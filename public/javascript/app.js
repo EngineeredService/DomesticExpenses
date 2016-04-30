1ac7178446c5b45a605907f7a246b3b52d628e7f
kisdtapaswini <kisd.tapaswini@gmail.com>
2016-04-30 21:56:23 +0530

    Added UI fro Login

---
+++ public/javascript/app.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('App', ['ngRoute'])
+        .config(config);
+
+    config.$inject = ['$routeProvider', '$locationProvider'];
+    function config($routeProvider, $locationProvider) {
+        $routeProvider
+           
+            .when('/', {
+                controller: 'LoginController',
+                templateUrl: 'login/login.view.html',
+                controllerAs: 'vm'
+            })
+
+            .when('/register', {
+                controller: 'RegisterController',
+                templateUrl: 'register/register.view.html',
+                controllerAs: 'vm'
+            })
+
+            .otherwise({ redirectTo: '/login' });
+    }
+
+
+
+})();
