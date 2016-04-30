803c3e6ded055f57340f0ea3cf4986d2f5d9e45f
Chittaranjan Swain <chittaranjan.swain@oracle.com>
2016-04-30 22:23:25 +0530

    Integration issue fixed

--- app.js
+++ app.js
@@ -38,6 +38,12 @@ app.use(bodyParser.urlencoded({
 // Use the passport package in our application
 app.use(passport.initialize());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
